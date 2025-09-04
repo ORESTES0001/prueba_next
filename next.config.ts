868b0eea01f6982f7d96e2c7a5a3bf266bb4520c
ORESTES0001 <75502802+ORESTES0001@users.noreply.github.com>
2025-09-04 16:41:40 -0600

    solucion postgres5

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   
-  turbopack: {
-    root: __dirname, // fuerza a usar la raíz correcta
-  },
-
-   experimental: {
-    ppr: 'incremental'
-  },
+  
 };
 
 export default nextConfig;
