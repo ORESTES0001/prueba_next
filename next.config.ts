0b2a1e343cbdd54067aa050763d9a3e23c97171b
ORESTES0001 <75502802+ORESTES0001@users.noreply.github.com>
2025-09-04 16:10:44 -0600

    solucion postgres2

--- next.config.ts
+++ next.config.ts
@@ -6,10 +6,7 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: __dirname, // fuerza a usar la raíz correcta
   },
-  allowedDevOrigins: [
-    'http://localhost:3000',
-    'http://192.168.1.70:3000',
-  ],
+
    experimental: {
     ppr: 'incremental'
   },
